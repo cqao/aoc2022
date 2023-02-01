fae146d8afe00630649e8667fa64fa30809ab640
cqao <60248247+cqao@users.noreply.github.com>
2023-02-01 11:56:25 -0500

    boilerplate

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,14 @@
+
+// Each ruck has 2 compartments
+// Each item is meant to go into only one of the compartments
+// Items identified by lowercase or uppercase letter
+// Each item has a priotiry assigned: 'a' through 'z' has priority values 1 - 26
+// Each item has a priotiry assigned: 'A' through 'Z' has priority values 27 - 52
+// Each ruck compartment shares an item
+
+// GOAL: Add the priority values for each shared component
+
+
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
@@ -12,6 +23,6 @@ fn main() {
     for line in f.lines() {
         let line = line.expect("Unable to read line");
 
-        println!(line);
+        println!("{}", line);
     }
 }
